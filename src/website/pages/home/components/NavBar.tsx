4bcc689b2e06747c1e302740db5b25768603fc10
charitra shrestha <stharabi9862187405@gmail.com>
2026-03-30 14:52:48 +0545

    feat(navbar): add support tickets link

--- src/website/pages/home/components/NavBar.tsx
+++ src/website/pages/home/components/NavBar.tsx
@@ -176,6 +176,8 @@ export function NavBar() {
         { name: "Contact List", link: "/contact-info" },
         { name: "Fee Planner", link: "/fee-payment-planners" },
         { name: "Payment Modes", link: "/payment-modes" },
+        { name: "Support Tickets", link: "https://support.lbef.info/" },
+
 
         {
           name: "Alumni",
